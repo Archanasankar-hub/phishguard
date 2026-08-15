b5be5a88383557d7719bef8c2482f05a0bbd5668
Archanasankar-hub <archanasankar2512@gmail.com>
2026-08-15 11:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ def analyze():
     return jsonify(result)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
